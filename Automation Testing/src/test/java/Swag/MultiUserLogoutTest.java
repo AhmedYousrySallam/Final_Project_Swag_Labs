2322ac2832f337d3e7476f042ffde5dae33f0b8a
Ahmed Yousry <106488566+AhmedYousrySallam@users.noreply.github.com>
2026-04-17 15:12:06 +0200

    Refactor multi user logout tests to use base test

--- Automation Testing/src/test/java/Swag/MultiUserLogoutTest.java
+++ Automation Testing/src/test/java/Swag/MultiUserLogoutTest.java
@@ -1,31 +1,16 @@
 package Swag;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.UnexpectedAlertBehaviour;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MultiUserLogoutTest {
-
-    private WebDriver driver;
-    private final String url = "https://www.saucedemo.com/";
-
-    // Login locators
-    private final By userNameLocator = By.id("user-name");
-    private final By passwordLocator = By.name("password");
-    private final By loginButtonLocator = By.id("login-button");
+public class MultiUserLogoutTest extends BaseTest {
 
     @DataProvider(name = "allUsersForLogout")
     public Object[][] allUsersForLogout() {
@@ -33,7 +18,7 @@ public class MultiUserLogoutTest {
         for (String u : LoginData.userName) {
             if (u == null) continue;
             String name = u.trim().toLowerCase();
-            if (name.equals("locked_out_user")) continue; // cannot log in to perform logout
+            if (name.equals("locked_out_user")) continue;
             allowed.add(u);
         }
         Object[][] data = new Object[allowed.size()][1];
@@ -43,53 +28,16 @@ public class MultiUserLogoutTest {
         return data;
     }
 
-    @BeforeMethod
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("profile.password_manager_enabled", false);
-        prefs.put("profile.default_content_setting_values.notifications", 2);
-        options.setExperimentalOption("prefs", prefs);
-        options.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, UnexpectedAlertBehaviour.DISMISS);
-        options.addArguments("--disable-notifications");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    private void login(String username) {
-        driver.get(url);
-        driver.findElement(userNameLocator).sendKeys(username);
-        driver.findElement(passwordLocator).sendKeys(LoginData.password);
-        driver.findElement(loginButtonLocator).click();
-        Wait<WebDriver> wait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(30))
-                .pollingEvery(Duration.ofMillis(100))
-                .ignoring(NoSuchElementException.class);
-        wait.until(d -> d.findElement(By.cssSelector("span.title")).isDisplayed());
-    }
-
     @Test(dataProvider = "allUsersForLogout", testName = "MU_LO_TC_01 - Logout works for all users")
     public void logoutWorksForAllUsers(String username) {
-        // Login as given user
-        login(username);
+        loginAs(username);
 
-        // Ensure we are on products page
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.urlContains("inventory.html"));
 
-        // Perform logout using dedicated page object
         LogoutPage logoutPage = new LogoutPage(driver);
         logoutPage.logout();
 
-        // Verify back on login page
         Assert.assertTrue(logoutPage.isOnLoginPage(), "User should be navigated back to login page after logout");
     }
 }
