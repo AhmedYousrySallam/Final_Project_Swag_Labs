a502299a35518170a5068677180cc467327a2ada
Ahmed Yousry <106488566+AhmedYousrySallam@users.noreply.github.com>
2026-04-17 15:07:50 +0200

    Refactor login page into proper page object

--- Automation Testing/src/main/java/Swag/LoginPage.java
+++ Automation Testing/src/main/java/Swag/LoginPage.java
@@ -2,89 +2,50 @@ package Swag;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-public class LoginPage{
 
-    //Drivers
-    //Variables
-    //By Locators
-    //Elements
-    //Methods
+public class LoginPage {
 
-    //Drivers
-    WebDriver myBrowser;
+    private final WebDriver driver;
+    private final String url = "https://www.saucedemo.com/";
 
+    private final By userNameLocator = By.id("user-name");
+    private final By passwordLocator = By.name("password");
+    private final By loginButtonLocator = By.id("login-button");
+    private final By errorMessageLocator = By.cssSelector("h3[data-test='error']");
 
-    //Variables
-    //String userName;
-    //String Pass = "secret_sauce";
-    String url = "https://www.saucedemo.com/";
-
-    //By Locators
-    By userNameLocator = By.id("user-name");
-    By passwordLocator = By.name("password");
-    By loginButtonLocator = By.id("login-button");
-    By errorMessage = By.cssSelector("h3[data-test='error']");
-
-
-   @BeforeMethod
-   public void start(){
-     //Create Driver
-     myBrowser = new ChromeDriver();
-     //Navigate To Url
-      myBrowser.get(url);
-     //Maximize the Window
-     myBrowser.manage().window().maximize();
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
     }
-    @Test(testName ="TC_01-Verify login works with correct credentials",priority = 1)
-    public void Standerd_User_Test(){
-       myBrowser.findElement(userNameLocator).sendKeys(LoginData.userName[0]);
-       myBrowser.findElement(passwordLocator).sendKeys(LoginData.password);
-       myBrowser.findElement(loginButtonLocator).click();
-       Assert.assertEquals("https://www.saucedemo.com/inventory.html",myBrowser.getCurrentUrl());
-       myBrowser.quit();
+
+    public void open() {
+        driver.get(url);
     }
-    @Test(testName ="TC_02-Verify login works with correct credentials",priority = 2)
-    public void locked_out_user_Test(){
-        myBrowser.findElement(userNameLocator).sendKeys(LoginData.userName[1]);
-        myBrowser.findElement(passwordLocator).sendKeys(LoginData.password);
-        myBrowser.findElement(loginButtonLocator).click();
-        Assert.assertEquals("Epic sadface: Sorry, this user has been locked out.",myBrowser.findElement(errorMessage).getText());
-        myBrowser.quit();
+
+    public void enterUsername(String username) {
+        driver.findElement(userNameLocator).clear();
+        driver.findElement(userNameLocator).sendKeys(username);
     }
-    @Test(testName ="TC_03-Verify login works with correct credentials",priority = 3)
-    public void problem_user_Test(){
-        myBrowser.findElement(userNameLocator).sendKeys(LoginData.userName[2]);
-        myBrowser.findElement(passwordLocator).sendKeys(LoginData.password);
-        myBrowser.findElement(loginButtonLocator).click();
-        Assert.assertEquals("https://www.saucedemo.com/inventory.html",myBrowser.getCurrentUrl());
-        myBrowser.quit();
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordLocator).clear();
+        driver.findElement(passwordLocator).sendKeys(password);
     }
-    @Test(testName ="TC_04-Verify login works with correct credentials",priority = 4)
-    public void performance_glitch_user_Test(){
-        myBrowser.findElement(userNameLocator).sendKeys(LoginData.userName[3]);
-        myBrowser.findElement(passwordLocator).sendKeys(LoginData.password);
-        myBrowser.findElement(loginButtonLocator).click();
-        Assert.assertEquals("https://www.saucedemo.com/inventory.html",myBrowser.getCurrentUrl());
-        myBrowser.quit();
+
+    public void clickLogin() {
+        driver.findElement(loginButtonLocator).click();
     }
-    @Test(testName ="TC_05-Verify login works with correct credentials",priority = 5)
-    public void error_user_Test(){
-        myBrowser.findElement(userNameLocator).sendKeys(LoginData.userName[4]);
-        myBrowser.findElement(passwordLocator).sendKeys(LoginData.password);
-        myBrowser.findElement(loginButtonLocator).click();
-        Assert.assertEquals("https://www.saucedemo.com/inventory.html",myBrowser.getCurrentUrl());
-        myBrowser.quit();
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
     }
-    @Test(testName ="TC_06-Verify login works with correct credentials",priority = 6)
-    public void visual_user_Test(){
-        myBrowser.findElement(userNameLocator).sendKeys(LoginData.userName[5]);
-        myBrowser.findElement(passwordLocator).sendKeys(LoginData.password);
-        myBrowser.findElement(loginButtonLocator).click();
-        Assert.assertEquals("https://www.saucedemo.com/inventory.html",myBrowser.getCurrentUrl());
-        myBrowser.quit();
+
+    public String getErrorMessage() {
+        return driver.findElement(errorMessageLocator).getText();
+    }
+
+    public boolean isLoginButtonDisplayed() {
+        return driver.findElement(loginButtonLocator).isDisplayed();
     }
 }
